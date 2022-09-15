702f02f98d7d36fce0f472e8b6ad46e42fae221c
renxiang-hu <49302420+renxiang-hu@users.noreply.github.com>
2022-09-15 20:26:44 +0800

    所有用于测试的所有案例

---
+++ src/main/java/Map/MapToList.java
@@ -0,0 +1,15 @@
+package Map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapToList {
+    public static void main(String[] args) {
+        Map<Integer,String> map = new HashMap<>();
+        map.put(1,"hurenxiang");
+        map.put(2,"caiyao");
+        map.put(3,"kebi");
+
+        //将
+    }
+}
